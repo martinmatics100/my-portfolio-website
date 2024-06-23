c3f611fa48d2df8d446155ccdfa9aabb5bd9a16d
Nwatu Martin Chukwuemeka <128225275+martinmatics100@users.noreply.github.com>
2024-06-23 20:53:35 +0100

    commit all

--- My_Portfolio/assets/js/main.js
+++ My_Portfolio/assets/js/main.js
@@ -246,6 +246,26 @@
     },
   });
 
+  // Form submission Script
+
+  const scriptURL =
+    "https://script.google.com/macros/s/AKfycbwMkXKbP0lKF4HUng_TePPIkwLkRaVf13e3ZxeL_jBciLi3rDbGykvbi2acrlBiiVVnbg/exec";
+  const form = document.forms["submit-to-google-sheet"];
+  const msg = document.getElementById("msg");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetch(scriptURL, { method: "POST", body: new FormData(form) })
+      .then((response) => {
+        msg.innerHTML = "Message sent successfully";
+        setTimeout(function () {
+          msg.innerHTML = "";
+        }, 5000);
+        form.reset();
+      })
+      .catch((error) => console.error("Error!", error.message));
+  });
+
   /**
    * Initiate Pure Counter
    */
